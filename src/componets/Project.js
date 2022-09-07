8788cb5c3bc216cd2463a1f99b19e610130eaa54
Alex Contreras <alex.code29@gmail.com>
2022-09-07 02:40:51 -0500

    style: add project images and fix padding, margin

--- src/componets/Project.js
+++ src/componets/Project.js
@@ -4,16 +4,18 @@ function Project({info}) {
     const {name , description, tech, link} = info
     
     return (
-        <Box border='2px' borderRadius='25' w={{base:'80vw', md: '35vw', lg:'18vw'}}  h={{base: 'inherit'}} m={4} _hover={{boxShadow:'dark-lg'}}>
+        <Box border='2px' borderRadius='25' w={{base:'80vw', md: '35vw', lg:'18vw'}} maxH='55vh' m={4} _hover={{boxShadow:'dark-lg'}}>
             <Image borderTopRadius='23' src={require(`../assets/project-img/${name}.png`)} />
-            <Box p={3}>
-                <Link href={`${link}`}>
-                    <Heading as='h3' fontSize='3xl'>{name}</Heading>
-                </Link>
+            <Box p={3} >
+                <Heading as='h3' fontSize='3xl'>
+                    <Link href={`${link}`}>
+                        {name}
+                    </Link>
+                </Heading>
                 <Text as='p' fontSize={{base:'md'}} my='1'>{description}</Text>
-                <Flex wrap='wrap'>
+                <Flex wrap='wrap' columnGap='5px'>
                     {tech.map((lang) => (
-                        <Tag p='2' m='1' border='1px' borderColor='grey' key={lang}>{lang}</Tag>
+                        <Tag p='2' my='1' border='1px' borderColor='grey' key={lang}>{lang}</Tag>
                     ))}
                 </Flex>
             </Box>
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -5,33 +5,45 @@ function Projects() {
     const projectInfo = [
         {
             name: 'GeekTutor',
-            description: 'GeekTutor is an app where users can view and schedule sessions with tutors. My team consisted of 4 members and we used Agile development methodology. I worked on the front-end to implement queries to display their data and the login/logout functionality',
-            tech: ['MERN', 'Javascript'],
+            description: 'GeekTutor is an app where users can view and schedule sessions with tutors. My team consisted of 4 members and we used the Agile development methodology. I worked on the front-end to implement queries to display their data and the login/logout functionality.',
+            tech: ['MERN', 'Javascript', 'Apollo', 'Email.js'],
             link: 'https://geektutor-123.herokuapp.com/'
         },
         {
             name: 'Bootleg',
-            description: 'Bootleg is an instagram clone that allow user to upload picture and also comment on other user post.My team consisted of 4 members and we used Agile development methodology. I worked on the front-end to implement handlebars, login/logout, and built the dashboard',
+            description: 'Bootleg is a web application that allows users to upload a picture and also comment on another user\'s post. Worked in a team of 4 following the agile development methodology. I implement handlebars, login/logout logic, and built the dashboard.',
             tech: ['Handlebars', 'CSS3','Javascript', 'Express', 'Sequelize'],
             link: 'https://group-project-bootleg.herokuapp.com/'
         },
         {
             name: 'One Tech Blog',
-            description: 'A web application where users can create a profile and write a blog about any topic. Built following the MVC design pattern. Users can also check out other user profiles and see all the blogs they have written.',
+            description: 'A web application where users can create a profile and write a blog about any topic. Built following the MVC design pattern. Users can also check other user profiles and see all the blogs they have written. Unit testing done on helper function to improve maintainability.',
             tech: ['Handlebars', 'CSS3', 'Javascript', 'Sequlize', 'Express', 'Jest'],
             link: 'https://onetechblog.herokuapp.com/'
         },
         {
             name: 'Budget Tracker',
-            description: 'Finace app where you can track your spending. It uses a service worker to be able to function offline. It\'s also a Progressive Web Application so u can download it onto your desktop or mobile device.',
+            description: 'Finance app where you can track your spending. App uses a service worker to be able to function offline. It\'s also a Progressive Web Application so users can download it onto their desktop or mobile device.',
             tech: ['Javascript', 'Express', 'Mongoose', 'PWA'],
             link: 'https://agile-forest-86640.herokuapp.com/'
+        },
+        {
+            name: 'noSQL Backend',
+            description: 'A social media application\'s backend. Designed REST apis to perform CRUD operations on the noSQL database',
+            tech: ['Javascript', 'Express','MongoDB', 'Mongooes'],
+            link: 'https://drive.google.com/file/d/143AhdeKcGKyDBXIridyw4-u2jVf4trBb/view'
+        },
+        {
+            name: 'Password Generator',
+            description: 'Web application that outputs a password base on the users answers',
+            tech: ['HTML5', 'CSS3', 'Javascript'],
+            link: 'https://alexurielcontreras.github.io/Password-Generator/'
         }
         
     ];
 
     return (
-        <Flex direction={'row'} flexWrap='wrap' align={{base:'center', md:'inherit'}} justifyContent={{base: 'center', md: 'start' }}  pt={{base: '10'}}>
+        <Flex direction={'row'} flexWrap='wrap' align={{base:'center', md:'inherit'}} justifyContent={{base: 'center', lg: 'start' }}  pt={{base: '10'}}>
             {projectInfo.map((info) => {
                 return <Project key={info.name} info={info} />
             })}
