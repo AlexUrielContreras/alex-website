00e7703e074de382c0250e30e0c0b43c03b8b3a9
Alex Contreras <alex.code29@gmail.com>
2022-11-14 13:57:06 -0600

    refactor: remove about me text from landing page and structured heading

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,24 @@
-import { Flex, Heading, Text, Box, Link} from '@chakra-ui/react'
+import { Heading, Box, Link} from '@chakra-ui/react'
 import Socials from '../componets/Socials'
 
-function About({ currentW }) {
+function About() {
     return (
-        <>
-            <Flex textAlign='center' justifyContent={{ base: 'center', md: 'space-between' }} mt={{ base: '7vh', md: '17vh' }}>
-                <Box m={{ base: 'auto', sm: '0' }}>
-                    <Heading as='h1' fontSize={{ base: '2.1rem', lg: '5xl' }}>Alex U. Contreras</Heading>
-                    <Heading as='h2' fontSize={{ base: '1.41rem', lg: '3xl' }} mt={3}>Full Stack Developer</Heading>
+        <Box as='section' align='center' mt={{ base: '20vh', md: '23vh'}}>
+            <Box>
+                <Heading as='h1' letterSpacing='2px' fontSize={{ base: '3rem', md: '4rem' , lg: '5rem' }}>Alex Contreras</Heading>
+                <Heading as='h2' fontSize={{ base: '1.6rem', md: '2rem', lg: '2.5rem' }}>Full Stack Developer</Heading>
+            </Box>
+
+            <Box pt={{base:'5'}} mt={{base: '5'}} borderTop='2px solid black'>
+                <Box fontSize={{base:'1.4rem', md: '2rem'}}>
+                    <Link href={require('../assets/files/alex_resume_v1.pdf')} download>Download my Resume</Link>
+                </Box> 
+
+                <Box mt={6}>
+                    <Socials /> 
                 </Box>
-                {currentW >= 770 ? <Socials /> : null}
-            </Flex>
-
-            <Flex >
-                <Text as='p' my={10} fontSize='lg'>
-                    Hello, My name is Alex Contreras and I'm a Full Stack Developer. I completed The University of Kansas Full Stack Developer Course were
-                    we learned a new language or tool every week and implemented it into a project. We also grouped up to plan, implement, and present 3 projects.
-                    Projects ranged from a simple website that grabs data from an API, to creating full-stack applications using the MERN tech stack.
-                    Being able to experience working in a team was very eye-opening, I learned that communication, flexibility, patience, and hard work are key to getting
-                    a product up and running. I am a hard worker and work well in a team environment. I am confident in my skills in a real-world setting.
-                </Text>
-            </Flex>
-            <Box textAlign='center' fontSize='xl' py={{base: '10', lg: '20' }}>
-                <Link href={require('../assets/files/alex_resume_v1.pdf')} download>Click Here to Download my Resume</Link>
-            </Box> 
-        </>
-
+            </Box>
+        </Box>
     )
 };
 
