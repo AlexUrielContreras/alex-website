ebdabdee5e82de7c8131c4c73029614865c06a63
Alex Contreras <alex.code29@gmail.com>
2022-11-15 12:09:59 -0600

    feat: added a button to the project cards with a link to the github repo

--- src/componets/ProjectBox.js
+++ src/componets/ProjectBox.js
@@ -1,7 +1,7 @@
 import { Box,Image,Heading, Text, Flex } from '@chakra-ui/react'
 
 function Project({info}) {
-    const {name , description, tech, link} = info
+    const {name , description, tech, liveSite, githubRepo} = info
     
     return (
         <Box border='2px' as='article' w={{base:'80vw', md: '39vw', xl:'20vw'}}  _hover={{boxShadow:'dark-lg'}}>
@@ -12,10 +12,10 @@ function Project({info}) {
                 </Heading>
                 <Text as='p' fontSize={{base:'md'}} my='1'>{description}</Text>
                 <Flex my='3' justifyContent='space-around'>
-                    <button  className='proj-link-btn'><a href={link}>Live site</a></button>
-                    <button  className='proj-link-btn'><a href={link}>Github Repo</a></button>
+                    <button  className='proj-link-btn'><a href={liveSite}>Live site</a></button>
+                    <button  className='proj-link-btn'><a href={githubRepo}>Github Repo</a></button>
                 </Flex>
-                <Flex wrap='wrap' columnGap='2'>
+                <Flex wrap='wrap' columnGap='3'>
                     {tech.map((lang) => (
                         <div key={lang}>{lang}</div>
                     ))}
