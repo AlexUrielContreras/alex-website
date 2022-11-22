6e62bf133daee9624eb8a4f4b0f8210c0fb28013
Alex Contreras <alex.code29@gmail.com>
2022-11-21 19:53:01 -0600

    refactor: remove all project info from project page file and moved to seprate file

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,52 +1,9 @@
-import ProjectBox from '../componets/ProjectBox'
-import { Flex } from '@chakra-ui/react'
+import ProjectBox from '../componets/ProjectBox';
+import projectInfo from '../data/projectInfo';
+
+import { Flex } from '@chakra-ui/react';
 
 function Projects() {
-    const projectInfo = [
-        {
-            name: 'GeekTutor',
-            description: 'GeekTutor is a web app where users can view and schedule sessions with tutors. Team of 4 following the Agile development methodology. I implement queries to display their data dynamicle on the webpage by querying the databse and the login/logout functionality.',
-            tech: ['React Bootstrap', 'MERN', 'Javascript', 'Apollo Servers', 'Email.js'],
-            liveSite: 'https://geektutor-123.herokuapp.com/',
-            githubRepo: 'https://github.com/AlexUrielContreras/GeekTutors'           
-        },
-        {
-            name: 'Bootleg',
-            description: 'Bootleg is a web application that allows users to upload a picture and also comment on another user\'s post. Worked in a team of 4 following the agile development methodology. I implement handlebars, login/logout logic, and built the dashboard.',
-            tech: ['Handlebars', 'CSS3','Javascript', 'Express', 'Sequelize', 'Sharp'],
-            liveSite: 'https://bootleg-clone.herokuapp.com/',
-            githubRepo: 'https://github.com/AlexUrielContreras/Bootleg'         
-        },
-        {
-            name: 'One Tech Blog',
-            description: 'A web application where users can create a profile and write a blog about any topic. Built following the MVC design pattern. Users can also check other user profiles and see all the blogs they have written. Unit testing done on helper function to improve maintainability.',
-            tech: ['Handlebars', 'CSS3', 'Javascript', 'Sequlize', 'Express', 'Jest'],
-            liveSite: 'https://onetechblog.herokuapp.com/',
-            githubRepo: 'https://github.com/AlexUrielContreras/one-tech-blog'
-        },
-        {
-            name: 'Budget Tracker',
-            description: 'Finance app where you can track your spending. App uses a service worker to be able to function offline. It\'s also a Progressive Web Application so users can download it onto their desktop or mobile device.',
-            tech: ['Javascript', 'Express', 'Mongoose', 'PWA'],
-            liveSite: 'https://agile-forest-86640.herokuapp.com/',
-            githubRepo: 'https://github.com/AlexUrielContreras/budget-tracker'
-        },
-        {
-            name: 'noSQL Backend',
-            description: 'A social media application\'s backend. Designed REST apis to perform CRUD operations on a noSQL database.',
-            tech: ['Javascript', 'Express','MongoDB', 'Mongooes'],
-            liveSite: 'https://drive.google.com/file/d/143AhdeKcGKyDBXIridyw4-u2jVf4trBb/view',
-            githubRepo: 'https://github.com/AlexUrielContreras/mongoose-backend'
-        },
-        {
-            name: 'Password Generator',
-            description: 'Web application that outputs a password base on the users answers.',
-            tech: ['HTML5', 'CSS3', 'Javascript'],
-            liveSite: 'https://alexurielcontreras.github.io/Password-Generator/',
-            githubRepo: 'https://github.com/AlexUrielContreras/Password-Generator'
-        }
-        
-    ];
 
     return (
         <Flex direction={'row'} flexWrap='wrap' gap='5' align={{base:'center', md:'inherit'}} justifyContent={{base: 'center', xl: 'start' }}  py={{base: '10'}}>
