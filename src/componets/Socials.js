2000e35b45d8d0b1c276590d524b47f5151f5a0b
Alex Contreras <alex.code29@gmail.com>
2023-04-17 22:17:37 -0500

    style: added hover state to social icon links and about page content

--- src/componets/Socials.js
+++ src/componets/Socials.js
@@ -2,8 +2,8 @@ import { Flex, Link, Image} from '@chakra-ui/react'
 function Socials() {
     return (
         <Flex alignItems='center' justifyContent='center'>  
-            <Link mx='4' rel='noopener noreference' href='https://www.linkedin.com/in/alex-contreras-788b55225/' target='_blank'><Image src={require('../assets/icons/linkedin.ico')} alt='Linkedin icon link'/></Link>             
-            <Link mx='4' rel='noopener noreference' href='https://github.com/AlexUrielContreras' target='_blank'><Image src={require('../assets/icons/github.ico')} alt='Github Icon link' /></Link>
+            <Link mx='4' className='social-icon' rel='noopener noreference' href='https://www.linkedin.com/in/alex-contreras-788b55225/' target='_blank'><Image src={require('../assets/icons/linkedin.ico')} alt='Linkedin icon link'/></Link>             
+            <Link mx='4' className='social-icon' rel='noopener noreference' href='https://github.com/AlexUrielContreras' target='_blank'><Image src={require('../assets/icons/github.ico')} alt='Github Icon link' /></Link>
         </Flex>
     )
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,11 @@ function About() {
     return (
         <Box as='section' align='center' mt={{ base: '20vh', md: '23vh'}}>
             <Box>
-                <Heading as='h1' fontWeight='bold' letterSpacing='2px' fonts='heading' fontSize={{ base: '3rem', md: '4rem' , lg: '5rem' }}>Alex Contreras</Heading>
-                <Heading as='h2' fontSize={{ base: '1.6rem', md: '2rem', lg: '2.5rem' }}>Full Stack Developer</Heading>
+                <Heading as='h1' fontWeight='bold' letterSpacing='2px' fonts='heading' fontSize={{ base: '3rem', md: '4rem' , lg: '5rem' }} className='fade-in'>Alex Contreras</Heading>
+                <Heading as='h2' fontSize={{ base: '1.6rem', md: '2rem', lg: '2.5rem' }} className='fade-in'>Full Stack Developer</Heading>
             </Box>
 
-            <Box pt={{base:'5'}} mt={{base: '5'}} borderTop='2px solid black'>
+            <Box className='fade-in' pt={{base:'5'}} mt={{base: '5'}} borderTop='2px solid black'>
                 <Box fontSize={{base:'1.4rem', md: '1.75rem'}}>
                     <Link href={require('../assets/files/alex_resume_v1.pdf')} download>Download my Resume</Link>
                 </Box> 
