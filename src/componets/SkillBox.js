7098315cf711f7d5e235345a1e8e24145b61b76d
Alex Contreras <alex.code29@gmail.com>
2023-04-19 01:42:43 -0500

    refactor: change skill divs to use flexbox and adjusted margin

--- src/componets/SkillBox.js
+++ src/componets/SkillBox.js
@@ -1,16 +1,16 @@
-import {Heading, Box, Grid, GridItem} from '@chakra-ui/react';
+import { Heading, Box, Flex } from '@chakra-ui/react';
 
 function SkillBox({title, tech}) {
     return (
         <Box w={{base: '80vw', lg: '35vw'}} mt={{base: '16'}} >
-                <Heading mb='5' fontSize={{base: '1.6rem' ,md: '2.5rem'}} borderBottom='2px'>{title}</Heading>
-                <ul>
-                    <Grid templateColumns='1fr 1fr'>
-                        {tech.map(techList => (
-                            <GridItem as='li' fontSize={{base: '1.2rem', md: '1.5rem'}} key={techList}>{techList}</GridItem>
-                        ))}
-                    </Grid>
-                </ul>
+            <Heading mb='5' fontSize={{base: '1.6rem' ,md: '2.5rem'}} borderBottom='2px'>{title}</Heading>
+            <ul>
+                <Flex wrap='wrap '>
+                    {tech.map(techList => (
+                        <Box as='li' mx='10' fontSize={{base: '1.2rem', md: '1.5rem'}} key={techList}>{techList}</Box>
+                    ))}
+                </Flex>
+            </ul>
         </Box>
     )
 };
--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -3,20 +3,18 @@ import SkillBox from '../componets/SkillBox';
 
 function Skills() {
     const tech = [
-                ['HTML5', 'CSS3', 'SaSS', 'Javascript', 'Node', 'Handlebars'], /* Languages  */
-                ['jQuery', 'React'], /* Libraries */
-                ['Bootstrap', 'Express.js'], /* Frameworks */
+                ['HTML5', 'CSS3', 'Sass', 'Bootstrap', 'Javascript', 'jQuery', 'React', 'Handlebars', 'JWT'], /* Frontend  */
+                ['Node', 'Express', 'Python', 'Npm'], /* Backend */
                 ['MySQL', 'MongoDB','Sequelize', 'Mongoose'], /*   Databases  */
-                ['Jest', 'Git', 'GitHub', 'Npm', 'Insomnia', 'Heroku'] /* Tools */
+                ['Jest', 'Git', 'GitHub', 'Postman', 'Heroku'] /* Tools */
             ];
 
     return (
         <Flex direction={{base: 'column', lg: 'row'}} justifyContent={{lg:'space-around'}} align={{base:'center', md:'baseline'}} mt={{lg: '5vh'}} pb='10' wrap='wrap' letterSpacing='2px'>
-            <SkillBox title={'Front-End'} tech={tech[0]} />  
-            <SkillBox title={'Libraries'} tech={tech[1]} />  
-            <SkillBox title={'Frameworks'} tech={tech[2]} /> 
-            <SkillBox title={'Database'} tech={tech[3]} />
-            <SkillBox title={'Tools'} tech={tech[4]} />
+            <SkillBox title={'Frontend'} tech={tech[0]} />  
+            <SkillBox title={'Backend'} tech={tech[1]} />
+            <SkillBox title={'Database'} tech={tech[2]} />
+            <SkillBox title={'Tools'} tech={tech[3]} />
         </Flex>
 
     )
